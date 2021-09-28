76650608a77a471e08abf4a839331a7712beb516
zenkilan <zenkilan@tencent.com>
2021-09-28 18:05:35 +0800

    更新测试用例

--- pkg/ipa/analyse_test.go
+++ pkg/ipa/analyse_test.go
@@ -7,8 +7,39 @@ import (
 )
 
 func TestGetBuildNumberFromIPA(t *testing.T) {
-	ipaLocalPath := "./test.ipa"
-	buildNumber, err := GetBuildNumberFromIPA(ipaLocalPath)
-	assert.Nil(t, err)
-	t.Logf("build number is->%s", buildNumber)
+	// 定义测试用例参数
+	type args struct {
+		ipaLocalPath string
+	}
+	// 定义测试用例预期结果
+	type want struct {
+		buildNumber string
+	}
+	// 定义并初始化测试用例
+	tests := []struct {
+		name string
+		args args
+		want want
+	}{
+		{
+			name: "Get the CFBundleVersion from Info.plist successfully",
+			args: args{
+				ipaLocalPath: "./test.ipa",
+			},
+			want: want{
+				buildNumber: "3412",
+			},
+		},
+	}
+	// 遍历用例
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			// 执行待测方法
+			buildNumber, err := GetBuildNumberFromIPA(tt.args.ipaLocalPath)
+			t.Logf("build number is->%s", buildNumber)
+			// 断言
+			assert.Nil(t, err)
+			assert.Equal(t, tt.want.buildNumber, buildNumber)
+		})
+	}
 }
